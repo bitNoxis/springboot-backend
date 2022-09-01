fe2daf8804710759f5e7f110125164e6aa81d0b7
Patrick <drakemilza@gmail.com>
2022-09-01 10:19:32 +0200

    Model Product erstellt Packages erstellt

---
+++ src/main/java/com/example/springbootbackend/model/Product.java
@@ -0,0 +1,11 @@
+package com.example.springbootbackend.model;
+
+import java.sql.SQLData;
+
+public class Product {
+    private long id;
+    private String productName;
+    private SQLData expirationDate;
+    private SQLData buyDate;
+    private String description;
+}
